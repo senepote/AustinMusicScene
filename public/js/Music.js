2ebbe726ffb25cf1fc5da93b7846d95ddb7e05d4
STEVE NEPOTE <senepote@gmail.com>
2018-08-19 23:07:40 -0500

    more css changes

--- public/js/Music.js
+++ public/js/Music.js
@@ -8,6 +8,22 @@ class Music extends React.Component {
           <p>words about music and stuff and things and and and and and and</p>
         </div>
 
+        <div className='eventsvenues'>
+          <ul>
+            <li>ACL</li>
+            <li>SXSW</li>
+            <li>Any given day of the week...</li>
+          </ul>
+        </div>
+
+        <div className='eventsvenues'>
+        <ul>
+          <li>Emos</li>
+          <li>Come and Take It</li>
+          <li>Elysium</li>
+        </ul>
+        </div>
+
         <Footer />
       </div>
     )
