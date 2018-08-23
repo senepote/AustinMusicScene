9973e1f1ad8ed110d03e7e819dcc1aae76b3c3e1
STEVE NEPOTE <senepote@gmail.com>
2018-08-23 14:49:09 -0500

    more styling

--- public/js/Info2.js
+++ public/js/Info2.js
@@ -3,7 +3,7 @@ class Info2 extends React.Component {
     return (
       <div className='info'>
         <p>Austin''s official motto is the "Live Music Capital of the World" because on any given night, one can find over one hundred venues showcasing a wide variety of free - live music performances.</p>
-        <img src="/img/Flaming-Lips.jpg" alt="Zilker" />
+        <img className="mainpage" src="/img/Flaming-Lips.jpg" alt="Zilker" />
         <p>Austin is also famed for the SXSW and the Austin City Limits (ACL) Music Festivals which showcase an eclectic array of live musical talent from across the globe. The greatest concentration of music venues in Austin are located around 6th Street, the Warehouse District, Downtown, Central East Austin, South Congress, the Red River District, the University of Texas, South Lamar, and South Austin.</p>
         <p>more more more more</p>
 
--- public/js/Info3.js
+++ public/js/Info3.js
@@ -3,7 +3,7 @@ class Info3 extends React.Component {
     return (
       <div className='info'>
         <p>Austin''s official motto is the "Live Music Capital of the World" because on any given night, one can find over one hundred venues showcasing a wide variety of free - live music performances.</p>
-        <img src="/img/InThisMoment.jpg" alt="maria" />
+        <img className="mainpage" src="/img/InThisMoment.jpg" alt="maria" />
         <p>Austin is also famed for the SXSW and the Austin City Limits (ACL) Music Festivals which showcase an eclectic array of live musical talent from across the globe. The greatest concentration of music venues in Austin are located around 6th Street, the Warehouse District, Downtown, Central East Austin, South Congress, the Red River District, the University of Texas, South Lamar, and South Austin.</p>
         <p>more more more more</p>
 
