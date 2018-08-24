6de5bd41463f9b88c4e2e04e27eff95507161d1e
STEVE NEPOTE <senepote@gmail.com>
2018-08-23 21:07:37 -0500

    adjusting css and adding more photos

--- public/js/Info3.js
+++ public/js/Info3.js
@@ -5,7 +5,7 @@ class Info3 extends React.Component {
         <p>Austin became renowned for nurturing talented musicians who did not fit the mold anywhere else, and were drawn in by the creative vibe, liberal politics, and the low cost-of-living in what was then a laid back Capital city whose largest employers were the University of Texas and State government. Austin''s reputation continued to grow and become celebrated for its folk, blues, jazz, bluegrass, tejano, zydeco, new wave, punk, and indie music scenes.</p>
         <img className="mainpage" src="/img/951391150-1024x1024.jpg" alt="maria" />
       <div>
-        <a href="https://www.theguardian.com/travel/2012/oct/17/top-10-live-music-venues-austin">Top 10 Venues</a>
+        <a className="venue-link" href="https://www.theguardian.com/travel/2012/oct/17/top-10-live-music-venues-austin">Top 10 Venues</a>
       </div>
 
 
--- public/js/Photos.js
+++ public/js/Photos.js
@@ -4,8 +4,24 @@ class Photos extends React.Component {
       <div className='eventsvenues'>
         <h2>Photos</h2>
         <img className='eventsvenues' src="/img/ACL-Art.jpg" alt="acl" />
+
         <img className='eventsvenues' src="/img/InThisMoment.jpg" alt="itm1" />
+
         <img className='eventsvenues' src="/img/ACL_2017_Poster.jpg" alt="ACL-17" />
+
+        <img className='eventsvenues' src="/img/Boy-Harsher.webp" alt="boyharsher" />
+
+        <img className='eventsvenues' src="/img/us&rhona.jpg" alt="us" />
+
+        <img className='eventsvenues' src="/img/MansonAustin.jpg" alt="manson" />
+
+        <img className='eventsvenues' src="/img/Mr-Kitty-1.jpg" alt="MrKitty1" />
+
+        <img className='eventsvenues' src="/img/SINE-1.jpg" alt="SINE1" />
+
+        <img className='eventsvenues' src="/img/Mr-Kitty-2.jpg" alt="Mrkitty2" />
+
+        <img className='eventsvenues' src="/img/SINE-Live-1.jpg" alt="SINE" />
       </div>
     )
   }
